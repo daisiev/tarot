3c708e46d800d268fc7b3a5b422f61f3da57731a
River Merrill <rivermerrill.me@gmail.com>
2019-05-02 15:58:39 -0600

    added home page with columns, mock data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public readings = [
+    {date: Date.now(), spread: '3card', cards: ['3 of Swords', 'Page of Wands', 'Death']},
+    {date: Date.now(), spread: '1Card', cards: ['The Emperor']},
+    {date: Date.now(), spread: '1Card', cards: ['The Tower']}
+  ];
+  constructor() {}
 
 }
